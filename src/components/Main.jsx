dc46a4e41f1489379f3f68df4a533c65b32fc2f9
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-11-26 21:27:43 +0500

    finish

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 import Hero from './Hero'
 import Services from './Services'
 import Provider from './Provider'
-import Mobile from './mobile'
+import Mobile from './Mobile'
 import Doctor from './Doctor'
 const Main = () => {
   return (
